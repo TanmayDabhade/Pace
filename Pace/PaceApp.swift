f2294c18de776da380518f4dc9bfb3751e3425e5
TanmayDabhade <tanmaydabhade129@gmail.com>
2024-10-28 23:50:11 -0400

    Initial Commit

---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by Tanmay Dabhade on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
